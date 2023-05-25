38ec2f527783a85fa2bab9c8a66412e5768e7e56
thisisamal <amalpullan4@gmail.com>
2023-05-25 09:23:58 +0530

    done

--- bot/views.py
+++ bot/views.py
@@ -11,21 +11,19 @@ client = Client(account_sid, auth_token)
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def mybot(request):
-   
-    
 # import openai
 
-    chat  = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": "Where was it played?"}
-        ]
-    )
-    replay = chat.choices[0].message.content
-    print(replay)
+    # chat  = openai.ChatCompletion.create(
+    # model="gpt-3.5-turbo",
+    # messages=[
+    #         {"role": "system", "content": "You are a helpful assistant."},
+    #         {"role": "user", "content": "Who won the world series in 2020?"},
+    #         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    #         {"role": "user", "content": "Where was it played?"}
+    #     ]
+    # )
+    # replay = chat.choices[0].message.content
+    # print(replay)
     return render(request,'index.html')
 
 @csrf_exempt
