2beb513c51f93961b70359ba733fd95830a1412c
thisisamal <amalpullan4@gmail.com>
2023-05-30 18:11:47 +0530

    done

--- bot/urls.py
+++ bot/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from .views import Download_exel,whatsupbot,webbot,Dashboard,Login,SignUp,Logout,bot_registration,chatbots
 urlpatterns = [
     path('',Login),
-    path('/whatsupbot',whatsupbot),
-    path('/webbot',webbot.as_view()),
-    path('/dashboard',Dashboard),
-    path('/login',Login),
-    path('/logout',Logout),
-    path('/signup',SignUp),
-    path('/bot_registration',bot_registration),
-    path('/chatbots',chatbots),
-    path('/download_excel',Download_exel),
+    path('whatsupbot',whatsupbot),
+    path('webbot',webbot.as_view()),
+    path('dashboard',Dashboard),
+    path('login',Login),
+    path('logout',Logout),
+    path('signup',SignUp),
+    path('bot_registration',bot_registration),
+    path('chatbots',chatbots),
+    path('download_excel',Download_exel),
 ]
 # openpyxl
