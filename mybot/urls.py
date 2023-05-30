33a613ac4286847050769f506d1fb4e88a3b93e1
thisisamal <amalpullan4@gmail.com>
2023-05-30 18:00:09 +0530

    done

--- mybot/urls.py
+++ mybot/urls.py
@@ -32,5 +32,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('bots', include('bot.urls')),
+    path('', include('bot.urls')),
 ]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
